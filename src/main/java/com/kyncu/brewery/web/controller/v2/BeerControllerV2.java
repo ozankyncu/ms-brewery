230b6c9d2f7b30b2489f7f781076b664f786e1e0
ozanko <ozankyncu@gmail.com>
2022-05-28 18:40:33 +0300

    Mapstruct configurated with Lombok

--- src/main/java/com/kyncu/brewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/com/kyncu/brewery/web/controller/v2/BeerControllerV2.java
@@ -2,6 +2,9 @@ package com.kyncu.brewery.web.controller.v2;
 
 import com.kyncu.brewery.services.v2.BeerServiceV2;
 import com.kyncu.brewery.web.model.v2.BeerDtoV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +12,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/api/v2/beer")
 @RestController
 public class BeerControllerV2 {
-    private final BeerServiceV2 beerServiceV2;
 
-    public BeerControllerV2(BeerServiceV2 beerServiceV2) {
-        this.beerServiceV2 = beerServiceV2;
-    }
+    private final BeerServiceV2 beerServiceV2;
 
     @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDtoV2> getBeer(@PathVariable("beerId") UUID beerId) {
@@ -25,8 +27,10 @@ public class BeerControllerV2 {
 
     @PostMapping
     public ResponseEntity handlePost(@RequestBody BeerDtoV2 beerDtoV2) {
-        BeerDtoV2 savedDto = beerServiceV2.saveBeer(beerDtoV2);
-        HttpHeaders headers = new HttpHeaders();
+        log.debug("in handle post");
+        val savedDto = beerServiceV2.saveBeer(beerDtoV2);
+
+        val headers = new HttpHeaders();
         // TODO: Add hostname to url
         headers.add("location", "/api/v2/beer" + savedDto.getId().toString());
         return new ResponseEntity(headers, HttpStatus.CREATED);
